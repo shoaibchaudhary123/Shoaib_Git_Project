c8146ae066e6bbe0527c431cae11ca5b21a62708
shoaibchaudhary123 <110779645+shoaibchaudhary123@users.noreply.github.com>
2022-09-10 16:39:58 +0530

    Add files via upload

---
+++ Sample_C.java
@@ -0,0 +1,11 @@
+package JavaProgramForSelenium;
+import JavaProgramForSelenium.Shoaib123;
+public class Sample_C {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Shoaib123 ob =new Shoaib123();
+		ob.name();
+	}
+
+}
---
+++ Shoaib123.java
@@ -0,0 +1,7 @@
+package JavaProgramForSelenium;
+
+public class Shoaib123 {
+public void name() {
+	System.out.print("Nawab Shoaib Chaudhary");
+}
+}
